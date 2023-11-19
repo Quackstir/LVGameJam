d58948d7fb3118a5b1de3e83f84f8f513f88b2b3
Quackstir <51137567+Quackstir@users.noreply.github.com>
2023-11-18 16:55:34 -0800

    Added the game
    -Added player movement
    -Added Player weapons
    -Created Enemy
    -Spawning enemy

---
+++ Weapon.gd
@@ -0,0 +1,31 @@
+extends Node2D
+
+class_name WeaponComponent
+
+@export var Bullet: PackedScene
+@export var bullet_speed: float = 100000000.0
+@onready var EndOfGun = $EndOfGun
+var isShooting: bool = false
+
+@export var Recoil = 5.0
+
+signal player_Fired_Bullet(Recoil)
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+	
+	
+func ShootRepeat():
+	while isShooting:
+		await get_tree().create_timer(0.5).timeout
+		shoot()
+		
+func shoot():
+	print("Firing Weapon")
+	var bullet_instance = Bullet.instantiate()
+	bullet_instance.global_position = EndOfGun.global_position
+	bullet_instance.rotation_degrees = rotation_degrees
+	bullet_instance.apply_central_impulse(Vector2(bullet_speed,0).rotated(global_rotation))
+	get_tree().get_root().add_child(bullet_instance)
+	emit_signal("player_Fired_Bullet", Recoil)
