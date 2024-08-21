537ec5627e37ed2c46eb8d010361d1fb236e5459
andrewdiaz20 <andrewdiazn20@gmail.com>
2024-08-20 18:16:40 -0700

    pause menu
    hi

--- Levels/Menu.gd
+++ Levels/Menu.gd
@@ -1,5 +1,7 @@
 extends Control
 @onready var hover_sound = %HoverSound
+@onready var options_button = $MarginContainer/HBoxContainer/VBoxContainer/Options_button as Button
+@onready var options_menu = $"CanvasLayer/Menu/MarginContainer/HBoxContainer/Options Menu"
 
 func _on_play_game_button_down():
 	get_tree().change_scene_to_file("res://Levels/Level.tscn")
@@ -22,3 +24,4 @@ func _on_play_game_mouse_entered():
 
 func _on_quit_game_mouse_entered():
 	hover_sound.play()
+
--- Levels/options_menu.gd
+++ Levels/options_menu.gd
@@ -1,13 +1,26 @@
-extends Control
+#class_name OptionsMenu
+#extends Control
+#
+#@onready var exit_button = $MarginContainer/VBoxContainer/Exit_button as button
+#
+#signal exit_options_menu
+#
+#func _ready():
+	#exit_button.button_down.connect(on_exit_pressed)
+	#set_pressed(false)
+#
+#func on_exit_pressed() -> void:
+	#exit_options_menu.emit()
+	#set_process(false)
 
-func _input(event):
-	if event.is_action_pressed("ui_cancel"):
-		if not self.is_visible_in_tree():
-			# Pause the game when the pause menu is not visible
-			self.show()
-			get_tree().paused = true
-		else:
-			self.hide()
-			get_tree().paused = false
-
+#func _input(event):
+	#if event.is_action_pressed("ui_cancel"):
+		#if not self.is_visible_in_tree():
+			## Pause the game when the pause menu is not visible
+			#self.show()
+			#get_tree().paused = true
+		#else:
+			#self.hide()
+			#get_tree().paused = false
+#
 
