8653ffb2347e38486236fe993ec55bd2cf936332
ReeseMireau <76988644+ReeseMireau@users.noreply.github.com>
2024-12-21 14:37:54 -0600

    Implemented the basic FMOD functionality for the enemies and the explosion SFX, making it so that walking sfx for ants and dragonflies play looping, and sound louder when closer and pan depending on their side of you, likewise for the explosion sound, as well as pause when the game is paused. I have more to do but I wanted to push what I've done so far.

--- Levels/Pause Menu.gd
+++ Levels/Pause Menu.gd
@@ -1,21 +1,27 @@
 extends Control
-  
+
 @onready var pause_menu = $"CanvasLayer/Pause Menu Panel"
 var paused = false
 
+@export var bus_asset: BusAsset
+var bus: Bus
 
 func _on_resume_game_button_down():
 	pauseMenu()
 
 func _ready():
 	pause_menu.hide()
+	bus = FMODStudioModule.get_studio_system().get_bus(bus_asset.path)
+	bus.set_paused(false)
 
 func pauseMenu():
 	if paused:
 		pause_menu.hide()
+		bus.set_paused(false)
 		Engine.time_scale = 1
 	else:
 		pause_menu.show()
+		bus.set_paused(true)
 		Engine.time_scale = 0
 	paused = !paused
 
--- Objects/Gameplay/Projectiles/explosion.gd
+++ Objects/Gameplay/Projectiles/explosion.gd
@@ -3,10 +3,35 @@ extends Node2D
 @onready var collision_shape_2d = $DamageBoxComponent/CollisionShape2D
 @onready var animation_player = $AnimationPlayer
 
+# should be the maximum distance an explosion could be assuming the numbers I was given was correct
+const MAXDIST = sqrt((2845*2845)+(1773*1773))
+# event 
+@export var event: EventAsset
+var instance: EventInstance
+
+func _enter_tree() -> void:
+	PlayFMODSound()
+
 func _on_timer_timeout():
 	collision_shape_2d.disabled = true
 	animation_player.play("Explosion")
 
-
-func _on_audio_stream_player_2d_finished():
-	queue_free()
+# Plays the explosion sound, adapting the parameters to where the player is at.
+func PlayFMODSound():
+	# gets the player position to position sound properly
+	var player_position = GM.gameManager.player.position
+	# gets distance to player compared to maximum distance it could be
+	var dist = (position.distance_to(player_position) / MAXDIST)
+	# gets loudness by having less distance equal higher volume
+	var eq = 1.0 - dist
+	# gets which side it should be on by having distance + direction determine it
+	var pan = eq / 2.0
+	if(player_position.x < position.x):
+		pan = 1.0 - pan;
+	# Creates the instance, sets the parameters for it, and then starts it.
+	instance = FMODRuntime.create_instance(event)
+	instance.start()
+	instance.set_parameter_by_name("EQ for Ant", eq, false)
+	instance.set_parameter_by_name("Panning for Enemy", pan, false)
+	instance.release()
+	
